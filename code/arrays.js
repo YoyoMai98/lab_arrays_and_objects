237f6b227d5ff852a87e4687c954aeeb634aa941
Yongran <maiyongran@gmail.com>
2022-09-20 13:12:35 +0100

    create lab files

---
+++ code/arrays.js
@@ -0,0 +1,87 @@
+/**
+ * In these tasks you will need to manipulate the array below in order to answer the questions. Some answers will require you to store the result in a variable, others will need you to inspect the whole array.
+ */
+
+stations = ["Dundee", "Edinburgh", "North Berwick", "Newcastle", "York", "Leeds", "Doncaster", "Peterborough"];
+
+/**
+ * Q1.
+ * The East Coast Main Line doesn't stop at Peterborough! Add London Kings Cross to the end of the array to complete the line.
+ */
+
+// console.log(stations);
+
+
+
+/**
+ * Q2.
+ * It doesn't go as far as Dundee either, so remove it from the start of the array.
+ */
+
+// console.log(stations);
+
+
+
+/**
+ * Q3.
+ * How many stations are on the line?
+ */
+
+stationCount = null;
+
+// console.log("There are " + stationCount + " stations.");
+
+
+
+/**
+ * Q4.
+ * Which station is the third stop?
+ */
+
+thirdStation = null;
+
+// console.log("The third station is " + thirdStation);
+
+
+
+/**
+ * Q5.
+ * Someone put the wrong Berwick on the timetable! Update "North Berwick" to "Berwick-upon-Tweed".
+ */
+
+// console.log(stations);
+
+
+
+/**
+ * Q6.
+ * How did Leeds get on there? Better remove it.
+ */
+
+// console.log(stations);
+
+
+
+/**
+ * Q7.
+ * We forgot about Darlington! Add it to the route between Newcastle and York.
+ */
+
+// console.log(stations);
+
+
+
+/**
+ * Q8.
+ * Is Durham one of the stops?
+ */
+
+stoppingAtDurham = null;
+
+// console.log(stoppingAtDurham);
+
+
+/**
+ * Q9.
+ * If we are stopping at Durham, print "Yay Durham!" to the terminal. If not print "Aww...".
+ */
---
+++ code/objects.js
@@ -0,0 +1,97 @@
+/**
+ * In these tasks you will need to manipulate the object below in order to answer the questions. Some answers will require you to store the result in a variable, others will need you to inspect the whole array.
+ */
+
+dog = {
+    name: "Poppy",
+    age: 4,
+    breed: "Springer Spaniel",
+    colours: ["white", "brown"],
+}
+
+cat = {
+    name: "Morris",
+    age: 9,
+    breed: "European Shorthair",
+    colours: ["black", "white"]
+}
+
+tortoise = {
+    name: "Ella",
+    age: 7,
+    breed: "Eastern Hermanns",
+    colours: ["yellow", "brown", "black"]
+}
+
+
+/**
+ * Q1. What's the name of the dog?
+ */
+
+dogName = null;
+
+// console.log("The dog's name is " + dogName);
+
+
+
+/**
+ * Q2. What breed is the cat?
+ */
+
+catBreed = null;
+
+// console.log("The cat is a " + catBreed);
+
+
+
+/**
+ * Q3. It's the tortoise's birthday! Add one to her current age.
+ */
+
+// console.log(tortoise)
+
+
+
+/**
+ * Q4. How many different colours is Poppy?
+ */
+
+dogColourCount = null;
+
+// console.log("The dog has " + dogColourCount + " different colours");
+
+
+
+/**
+ * Q5. I've just bought a 12-year-old African Grey parrot called Lucky. Create an object for her.
+ */
+
+parrot = null;
+
+// console.log(parrot);
+
+
+
+/**
+ * Q6. Ella's favourite food is dandelion, add this information to her object
+ */
+
+// console.log(tortoise);
+
+
+
+/**
+ * Q7. What's the combined age of all the pets?
+ */
+
+totalPetAge = null;
+
+// console.log("The total age of the pets is " + totalPetAge);
+
+
+
+/**
+ * Q8. It's turns out that Morris isn't a European Shorthair. We don't actually know his real breed, so remove that key from his object
+ */
+
+// console.log(cat);
